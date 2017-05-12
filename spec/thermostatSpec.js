6c53365b37569518fa01e93e2d87224cf8f44187
Alex Law <agent_law@hotmail.com>
2017-05-12 11:21:45 +0100

    Add power saving toggle and tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -25,8 +25,17 @@ describe('Thermostat', function() {
     expect( function() { thermostat.decrease() } ).toThrow('Can not go below 10 degrees');
   });
 
+  it('has power saving mode on', function() {
+    expect(thermostat.powerSavingOn).toBe(true);
+  });
+
   it('when saving mode is on has maximum temperature of 25 degrees', function() {
-    expect(thermostat.maximum_temp).toBe(25);
+    expect(thermostat.MAX_TEMP).toBe(25);
+  });
+
+  it('when saving mode is off has maximum temperature of 32 degrees', function() {
+    thermostat.powerSavingToggle();
+    expect(thermostat.MAX_TEMP).toBe(32);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,8 @@
 function Thermostat() {
   this.temp = 20;
   this.MIN_TEMP = 10;
+  this.MAX_TEMP = 25;
+  this.powerSavingOn = true;
 };
 
 Thermostat.prototype.increase = function() {
@@ -8,14 +10,22 @@ Thermostat.prototype.increase = function() {
 };
 
 Thermostat.prototype.decrease = function() {
-  this.minimum_temp();
+  this.minimumTemp();
   this.temp = this.temp - 1;
 };
 
-Thermostat.prototype.minimum_temp = function() {
+Thermostat.prototype.minimumTemp = function() {
   if (this.temp <= this.MIN_TEMP) {
-    throw 'Can not go below 10 degrees'
+    throw 'Can not go below 10 degrees';
   }
 };
 
-// Thermostat.prototype.maximum_temp = function()
+Thermostat.prototype.powerSavingToggle = function() {
+  this.powerSavingOn = !this.powerSavingOn;
+  if (this.powerSavingOn === true) {
+      this.MAX_TEMP = 25;
+  }
+  else {
+    this.MAX_TEMP = 32;
+  }
+};
